f5427d423da761a4d1076cedebb77907d579013e
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2020-04-10 21:31:23 +0800

    Add container for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {
     MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBNavItem, MDBCollapse, MDBDropdown,
-    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBBox
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBBox, MDBContainer
 } from "mdbreact";
 
 class Header extends Component {
@@ -33,32 +33,34 @@ class Header extends Component {
     render() {
         return (
             <MDBNavbar color="indigo" dark expand="md">
-                <MDBNavbarBrand>
-                    <strong className="white-text">Navbar</strong>
-                </MDBNavbarBrand>
-                <MDBNavbarToggler onClick={this.toggleCollapse} />
-                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                    <MDBNavbarNav left>
-                        {/* <MDBNavItem active>
-                            <MDBNavLink to="dashboard">Dashboard</MDBNavLink>
-                        </MDBNavItem> */}
-                        <li className="nav-item active">
-                            <a className="nav-link waves-effect waves-light" href="/dashboard">Dashboard</a>
-                        </li>
-                    </MDBNavbarNav>
-                    <MDBNavbarNav right>
-                        <MDBNavItem>
-                            <MDBDropdown>
-                                <MDBDropdownToggle nav caret>
-                                    <MDBBox tag="span" className="nav-usr-name">{this.props.name}</MDBBox>
-                                </MDBDropdownToggle>
-                                <MDBDropdownMenu className="dropdown-default">
-                                    <MDBDropdownItem onClick={this.handleLogout.bind()}>Logout</MDBDropdownItem>
-                                </MDBDropdownMenu>
-                            </MDBDropdown>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                </MDBCollapse>
+                <MDBContainer>
+                    <MDBNavbarBrand>
+                        <strong className="white-text">Navbar</strong>
+                    </MDBNavbarBrand>
+                    <MDBNavbarToggler onClick={this.toggleCollapse} />
+                    <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                        <MDBNavbarNav left>
+                            {/* <MDBNavItem active>
+                                <MDBNavLink to="dashboard">Dashboard</MDBNavLink>
+                            </MDBNavItem> */}
+                            <li className="nav-item active">
+                                <a className="nav-link waves-effect waves-light" href="/dashboard">Dashboard</a>
+                            </li>
+                        </MDBNavbarNav>
+                        <MDBNavbarNav right>
+                            <MDBNavItem>
+                                <MDBDropdown>
+                                    <MDBDropdownToggle nav caret>
+                                        <MDBBox tag="span" className="nav-usr-name">{this.props.name}</MDBBox>
+                                    </MDBDropdownToggle>
+                                    <MDBDropdownMenu className="dropdown-default">
+                                        <MDBDropdownItem onClick={this.handleLogout.bind()}>Logout</MDBDropdownItem>
+                                    </MDBDropdownMenu>
+                                </MDBDropdown>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
             </MDBNavbar>
         )
     }
